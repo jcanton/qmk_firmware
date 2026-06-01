151ec760d984cebbcd2b4455114b0b31630a9bf6
Jacopo <jacopo.canton@gmail.com>
2026-06-01 20:03:34 +0200

    some changes to keymap

--- keyboards/sofle_choc/keymaps/jcanton/keymap.c
+++ keyboards/sofle_choc/keymaps/jcanton/keymap.c
@@ -18,9 +18,9 @@
 
 enum layer_names {
     _QWERTY,
+    _SYMB,
     _NAV,
     _LEDS,
-    _ADJUST
 };
 
 enum custom_keycodes {
@@ -36,41 +36,41 @@ static bool layer_changed = false;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
-    KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-    KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_MUTE,   KC_MPLY,KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                      KC_LCTL, KC_LOPT, KC_LCMD, MO(_NAV), KC_ENT,  KC_SPC, MO(_LEDS), KC_RCMD, KC_ROPT, KC_RCTL
+    KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+    KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,      KC_MUTE,  KC_MPLY, KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                      KC_LCTL, KC_LOPT, KC_LCMD, MO(_SYMB), KC_ENT,   KC_SPC,  MO(_NAV), KC_RCMD, KC_ROPT, KC_RCTL
+),
+[_SYMB] = LAYOUT(
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
+    _______, KC_BRMU, _______, _______, _______, _______,                    _______, KC_LBRC, KC_RBRC, _______, _______, _______,
+    _______, KC_BRMD, _______, _______, _______, _______,                    _______, KC_LPRN, KC_RPRN, KC_LT,   KC_GT,   KC_QUOT,
+    _______, KC_CAPS, _______, _______, _______, _______, _______,  _______, _______, KC_LCBR, KC_RCBR, _______, _______, _______,
+                      _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______
 ),
 [_NAV] = LAYOUT(
-    KC_GRV,   KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                    KC_F6,  KC_F7,   KC_F8,   KC_MINS, KC_EQL,  KC_DEL,
-    KC_TAB,   XXXXXXX,XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX,                  XXXXXXX,XXXXXXX, MS_UP,   KC_LBRC, KC_RBRC, XXXXXXX,
-    KC_LCTL,  KC_BRMU,KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX,                  XXXXXXX,MS_LEFT, MS_DOWN, MS_RGHT, XXXXXXX, XXXXXXX,
-    KC_LSFT,  KC_BRMD,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_LOCK, KC_F3, XXXXXXX,MS_BTN1, MS_BTN2, XXXXXXX, XXXXXXX, KC_RSFT,
+    _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, KC_VIZ_TOGGLE,
+    KC_TAB,  _______, _______, KC_UP,   XXXXXXX, XXXXXXX,                  XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_LCTL, KC_BRMU, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX,                  XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
+    KC_LSFT, KC_BRMD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LOCK,  KC_F3, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
                       KC_LCTL, KC_LOPT, KC_LCMD, _______, KC_ENT,   KC_SPC, _______, KC_RCTL, KC_ROPT, KC_RCMD
 ),
 [_LEDS] = LAYOUT(
-    _______, _______,  _______,  _______, _______, _______,                   _______, _______, _______, _______, _______, KC_VIZ_TOGGLE,
+    _______, _______, _______,  _______, _______, _______,                   _______, _______, _______, _______, _______, KC_VIZ_TOGGLE,
     _______, RM_NEXT, RM_PREV, RM_HUEU, RM_HUED, RM_VALU,                  RM_VALD, RM_SATU, RM_SATD, RM_SPDU, RM_SPDD, RM_TOGG,
     _______, KC_LED_MODE, _______, _______, _______, _______,                _______, KC_LED_MODE, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______,
                       _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______
 ),
-[_ADJUST] = LAYOUT(
-    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-    _______, KC_INS,  KC_PSCR, KC_APP,  XXXXXXX, XXXXXXX,                  KC_PGUP, _______, KC_UP,   _______, _______, KC_BSPC,
-    _______, KC_LOPT, KC_LCTL, KC_LSFT, XXXXXXX, KC_CAPS,                  KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,  KC_BSPC,
-    _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE,XXXXXXX,  _______, _______,XXXXXXX, _______, XXXXXXX, _______, XXXXXXX, _______,
-                      _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______
-)
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_QWERTY] = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD), ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
-    [_NAV]     = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
-    [_LEDS]    = { ENCODER_CCW_CW(RM_VALD, RM_VALU), ENCODER_CCW_CW(KC_LED_MODE, RM_NEXT) },
-    [_ADJUST]  = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
+    [_SYMB]   = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD), ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
+    [_NAV]    = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
+    [_LEDS]   = { ENCODER_CCW_CW(RM_VALD, RM_VALU), ENCODER_CCW_CW(KC_LED_MODE, RM_NEXT) },
 };
 #endif
 
@@ -85,14 +85,14 @@ static void render_master_status(void) {
         case _QWERTY:
             oled_write_ln_P(PSTR("QWRTY"), false);
             break;
+        case _SYMB:
+            oled_write_ln_P(PSTR("SYMB "), false);
+            break;
         case _NAV:
             oled_write_ln_P(PSTR("NAV  "), false);
             break;
         case _LEDS:
             oled_write_ln_P(PSTR("LEDS "), false);
-            break;
-        case _ADJUST:
-            oled_write_ln_P(PSTR("ADJST"), false);
             break;
         default:
             oled_write_ln_P(PSTR("?????"), false);
@@ -104,14 +104,14 @@ static void render_master_status(void) {
         case _QWERTY:
             oled_write_ln_P(PSTR("Base "), false);
             break;
+        case _SYMB:
+            oled_write_ln_P(PSTR("Symb "), false);
+            break;
         case _NAV:
             oled_write_ln_P(PSTR("Nav  "), false);
             break;
         case _LEDS:
             oled_write_ln_P(PSTR("LEDs "), false);
-            break;
-        case _ADJUST:
-            oled_write_ln_P(PSTR("Adjst"), false);
             break;
         default:
             oled_write_ln_P(PSTR("?????"), false);
