63df3dbb5165e3f268516b8233d1fc3a6ec5a95f
Jacopo <jacopo.canton@gmail.com>
2026-06-11 13:27:50 +0200

    hold vs tap

--- keyboards/sofle_choc/keymaps/jcanton/keymap.c
+++ keyboards/sofle_choc/keymaps/jcanton/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                         KC_Y,       KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
     KC_LCTL,  KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                         KC_H,       KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
     KC_LSFT,  KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,      KC_MUTE,  KC_MPLY, KC_N,       KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                      KC_GRV, KC_LOPT, KC_LCMD, MO(_SYMB), KC_ENT,   KC_SPC,  KC_NAVLEDS, KC_RCMD, KC_LBRC, KC_RBRC
+                      MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_SYMB), KC_ENT,   KC_SPC,  KC_NAVLEDS, KC_RCMD, MT(KC_ROPT, KC_LBRC), MT(KC_RCTL, KC_RBRC)
 ),
 [_SYMB] = LAYOUT(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F6,   KC_F7,   KC_F8,   KC_MINS, KC_EQL,  KC_DEL,
