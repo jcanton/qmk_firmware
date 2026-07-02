fe158d05fc52ee8f07e9b9fcbf852bf74c659575
Jacopo <jacopo.canton@gmail.com>
2026-07-02 21:29:26 +0200

    more swapping

--- keyboards/sofle_choc/keymaps/jcanton/keymap.c
+++ keyboards/sofle_choc/keymaps/jcanton/keymap.c
@@ -45,14 +45,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                                 KC_Y,       KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
     KC_LCTL,  KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                                 KC_H,       KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
     KC_LSFT,  KC_Z,   KC_X,   KC_C,    KC_V,    KC_B, KC_MUTE,               KC_MPLY, KC_N,       KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), LT(_MOUSE, KC_ENT), LT(_NUM, KC_SPC), KC_FUNLEDS, MO(_SYM), MT(MOD_RALT, KC_LBRC), MT(MOD_RCTL, KC_RBRC)
+              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), LT(_MOUSE, KC_ENT), LT(_NUM, KC_SPC), MO(_SYM), KC_FUNLEDS, MT(MOD_RALT, KC_LBRC), MT(MOD_RCTL, KC_RBRC)
 ),
 [_NAV] = LAYOUT(
     _______, _______, _______, _______, _______, _______,                    _______, _______, _______, KC_MINS, KC_EQL,  KC_VIZ_TOGGLE,
     _______, _______, _______, _______, _______, _______,                    _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), C(KC_Y),
     _______, _______, _______, _______, _______, _______,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_CAPS,
     _______, _______, _______, _______, _______, _______, _______,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, KC_INS,
-              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), KC_ENT,          KC_SPC,  KC_FUNLEDS, MO(_NUM), MT(MOD_RALT, KC_LBRC), MT(MOD_RGUI, KC_RBRC)
+              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), KC_ENT,          KC_SPC , MO(_SYM), KC_FUNLEDS, MT(MOD_RALT, KC_LBRC), MT(MOD_RCTL, KC_RBRC)
 ),
 [_MOUSE] = LAYOUT(
     _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
@@ -66,28 +66,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_GRV,  KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC,                    _______, _______, _______, _______, _______, KC_BSLS,
     _______, KC_TILD, KC_LPRN, KC_RPRN, KC_LT,   KC_GT,                      _______, _______, _______, _______, _______, KC_QUES,
     _______, KC_LSFT, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  _______, _______, _______, _______, _______, _______, _______,
-              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), KC_ENT,      KC_SPC,  KC_FUNLEDS, MO(_NUM), MT(MOD_RALT, KC_LBRC), MT(MOD_RGUI, KC_RBRC)
+              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), KC_ENT,      KC_SPC , MO(_SYM), KC_FUNLEDS, MT(MOD_RALT, KC_LBRC), MT(MOD_RCTL, KC_RBRC)
 ),
 [_FUN] = LAYOUT(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
     _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, KC_F12,
     _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
-              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), KC_ENT,      KC_SPC,  KC_FUNLEDS, MO(_NUM), MT(MOD_RALT, KC_LBRC), MT(MOD_RGUI, KC_RBRC)
+              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), KC_ENT,      KC_SPC , MO(_SYM), KC_FUNLEDS, MT(MOD_RALT, KC_LBRC), MT(MOD_RCTL, KC_RBRC)
 ),
 [_NUM] = LAYOUT(
     _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
     _______, KC_7,    KC_8,    KC_9,    KC_MINS, KC_PLUS,                    _______, _______, _______, _______, _______, _______,
     _______, KC_4,    KC_5,    KC_6,    KC_ASTR, KC_SLSH,                    _______, _______, _______, _______, _______, _______,
     _______, KC_1,    KC_2,    KC_3,    KC_0,    KC_DOT,  _______,  _______, _______, _______, _______, _______, _______, _______,
-              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), KC_ENT,      KC_SPC,  KC_FUNLEDS, MO(_NUM), MT(MOD_RALT, KC_LBRC), MT(MOD_RGUI, KC_RBRC)
+              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), KC_ENT,      KC_SPC , MO(_SYM), KC_FUNLEDS, MT(MOD_RALT, KC_LBRC), MT(MOD_RCTL, KC_RBRC)
 ),
 [_LEDS] = LAYOUT(
     _______, _______,     _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
     _______, KC_LED_MODE, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
     _______, RM_NEXT,     RM_PREV, RM_HUEU, RM_HUED, RM_VALU,                    RM_VALD, RM_SATU, RM_SATD, RM_SPDU, RM_SPDD, RM_TOGG,
     _______, _______,     _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
-              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), KC_ENT,      KC_SPC,  KC_FUNLEDS, MO(_NUM), MT(MOD_RALT, KC_LBRC), MT(MOD_RGUI, KC_RBRC)
+              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), KC_ENT,      KC_SPC , MO(_SYM), KC_FUNLEDS, MT(MOD_RALT, KC_LBRC), MT(MOD_RCTL, KC_RBRC)
 ),
 };
 
