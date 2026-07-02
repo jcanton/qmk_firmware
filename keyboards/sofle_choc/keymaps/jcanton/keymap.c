31a75220a5063d2a778f9571bdc9086a663350f4
Jacopo <jacopo.canton@gmail.com>
2026-07-02 21:25:40 +0200

    and swap these two

--- keyboards/sofle_choc/keymaps/jcanton/keymap.c
+++ keyboards/sofle_choc/keymaps/jcanton/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                                 KC_Y,       KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
     KC_LCTL,  KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                                 KC_H,       KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
     KC_LSFT,  KC_Z,   KC_X,   KC_C,    KC_V,    KC_B, KC_MUTE,               KC_MPLY, KC_N,       KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), LT(_MOUSE, KC_ENT), LT(_SYM, KC_SPC), KC_FUNLEDS, MO(_NUM), MT(MOD_RALT, KC_LBRC), MT(MOD_RCTL, KC_RBRC)
+              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), LT(_MOUSE, KC_ENT), LT(_NUM, KC_SPC), KC_FUNLEDS, MO(_SYM), MT(MOD_RALT, KC_LBRC), MT(MOD_RCTL, KC_RBRC)
 ),
 [_NAV] = LAYOUT(
     _______, _______, _______, _______, _______, _______,                    _______, _______, _______, KC_MINS, KC_EQL,  KC_VIZ_TOGGLE,
