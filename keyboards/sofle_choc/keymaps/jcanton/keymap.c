585fa4e41ebcb6dd7f51a3528574ddc667cb642b
Jacopo <jacopo.canton@gmail.com>
2026-07-02 20:15:24 +0200

    refactor(sofle_choc): remove NAV Tab layer-tap and add shift shortcuts
    - Left inner thumb is now MO(_NAV), removing tap-hold lag since Tab
      is available on the dedicated Tab key.
    - Second right thumb is now LT(_FUN, KC_BSPC): real Backspace tap.
    - Shift+Backspace sends Delete.
    - LEDS layer toggle moved to Shift+Delete.

--- keyboards/sofle_choc/keymaps/jcanton/keymap.c
+++ keyboards/sofle_choc/keymaps/jcanton/keymap.c
@@ -30,14 +30,12 @@ enum custom_keycodes {
     KC_LOCK = QK_USER,
     KC_LED_MODE,
     KC_VIZ_TOGGLE,
-    KC_FUNLEDS,
 };
 
 static const uint8_t led_modes[] = {LED_FLAG_ALL, LED_FLAG_KEYLIGHT, LED_FLAG_UNDERGLOW, 0};
 static const uint8_t led_mode_count = sizeof(led_modes) / sizeof(led_modes[0]);
 static uint8_t led_mode_idx = 0;
 static bool layer_changed = false;
-static bool funleds_shift_used = false;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
@@ -45,49 +43,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                                 KC_Y,       KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
     KC_LCTL,  KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                                 KC_H,       KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
     KC_LSFT,  KC_Z,   KC_X,   KC_C,    KC_V,    KC_B, KC_MUTE,               KC_MPLY, KC_N,       KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, LT(_NAV, KC_TAB), LT(_MOUSE, KC_ENT), LT(_SYM, KC_SPC), KC_FUNLEDS, LT(_NUM, KC_DEL), MT(MOD_RALT, KC_LBRC), MT(MOD_RCTL, KC_RBRC)
+              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), LT(_MOUSE, KC_ENT), LT(_SYM, KC_SPC), LT(_FUN, KC_BSPC), LT(_NUM, KC_DEL), MT(MOD_RALT, KC_LBRC), MT(MOD_RCTL, KC_RBRC)
 ),
 [_NAV] = LAYOUT(
     _______, _______, _______, _______, _______, _______,                    _______, _______, _______, KC_MINS, KC_EQL,  KC_VIZ_TOGGLE,
     _______, _______, _______, _______, _______, _______,                    _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), C(KC_Y),
     _______, _______, _______, _______, _______, _______,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_CAPS,
     _______, _______, _______, _______, _______, _______, _______,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, KC_INS,
-              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, KC_TAB, KC_ENT,        KC_SPC,  KC_BSPC, KC_DEL,  MT(MOD_RALT, KC_LBRC), MT(MOD_RGUI, KC_RBRC)
+              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), KC_ENT,      KC_SPC,  KC_BSPC, KC_DEL,  MT(MOD_RALT, KC_LBRC), MT(MOD_RGUI, KC_RBRC)
 ),
 [_MOUSE] = LAYOUT(
     _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,                    _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), C(KC_Y),
     _______, _______, _______, _______, _______, _______,                    MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,  _______, MS_WHLL, MS_WHLU, MS_WHLD, MS_WHLR, _______, _______,
-              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, KC_TAB, KC_ENT,        MS_BTN1, MS_BTN2, MS_BTN3, MT(MOD_RALT, KC_LBRC), MT(MOD_RGUI, KC_RBRC)
+              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), KC_ENT,      MS_BTN1, MS_BTN2, MS_BTN3, MT(MOD_RALT, KC_LBRC), MT(MOD_RGUI, KC_RBRC)
 ),
 [_SYM] = LAYOUT(
     _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
     _______, KC_GRV,  KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC,                    _______, _______, _______, _______, _______, KC_BSLS,
     _______, KC_TILD, KC_LPRN, KC_RPRN, KC_LT,   KC_GT,                      _______, _______, _______, _______, _______, KC_QUES,
     _______, KC_LSFT, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  _______, _______, _______, _______, _______, _______, _______,
-              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, KC_TAB, KC_ENT,        KC_SPC,  KC_BSPC, KC_DEL,  MT(MOD_RALT, KC_LBRC), MT(MOD_RGUI, KC_RBRC)
+              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), KC_ENT,      KC_SPC,  KC_BSPC, KC_DEL,  MT(MOD_RALT, KC_LBRC), MT(MOD_RGUI, KC_RBRC)
 ),
 [_FUN] = LAYOUT(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
     _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, KC_F12,
     _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
-              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, KC_TAB, KC_ENT,        KC_SPC,  KC_BSPC, KC_DEL,  MT(MOD_RALT, KC_LBRC), MT(MOD_RGUI, KC_RBRC)
+              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), KC_ENT,      KC_SPC,  KC_BSPC, KC_DEL,  MT(MOD_RALT, KC_LBRC), MT(MOD_RGUI, KC_RBRC)
 ),
 [_NUM] = LAYOUT(
     _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
     _______, KC_7,    KC_8,    KC_9,    KC_MINS, KC_PLUS,                    _______, _______, _______, _______, _______, _______,
     _______, KC_4,    KC_5,    KC_6,    KC_ASTR, KC_SLSH,                    _______, _______, _______, _______, _______, _______,
     _______, KC_1,    KC_2,    KC_3,    KC_0,    KC_DOT,  _______,  _______, _______, _______, _______, _______, _______, _______,
-              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, KC_TAB, KC_ENT,        KC_SPC,  KC_BSPC, KC_DEL,  MT(MOD_RALT, KC_LBRC), MT(MOD_RGUI, KC_RBRC)
+              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), KC_ENT,      KC_SPC,  KC_BSPC, KC_DEL,  MT(MOD_RALT, KC_LBRC), MT(MOD_RGUI, KC_RBRC)
 ),
 [_LEDS] = LAYOUT(
     _______, _______,     _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
     _______, KC_LED_MODE, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
     _______, RM_NEXT,     RM_PREV, RM_HUEU, RM_HUED, RM_VALU,                    RM_VALD, RM_SATU, RM_SATD, RM_SPDU, RM_SPDD, RM_TOGG,
     _______, _______,     _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
-              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, KC_TAB, KC_ENT,            KC_SPC,  KC_FUNLEDS, KC_DEL,  MT(MOD_RALT, KC_LBRC), MT(MOD_RGUI, KC_RBRC)
+              MT(MOD_LCTL, KC_GRV), KC_LOPT, KC_LCMD, MO(_NAV), KC_ENT,          KC_SPC,  KC_BSPC, KC_DEL,  MT(MOD_RALT, KC_LBRC), MT(MOD_RGUI, KC_RBRC)
 ),
 };
 
@@ -95,10 +93,10 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LT(_MOUSE, KC_ENT):
             return 220;
-        case LT(_NAV, KC_TAB):
         case LT(_NUM, KC_DEL):
             return 180;
         case LT(_SYM, KC_SPC):
+        case LT(_FUN, KC_BSPC):
             return 130;
         default:
             return TAPPING_TERM;
@@ -222,21 +220,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 raw_hid_send(response, 32);
             }
             return false;
-        case KC_FUNLEDS:
-            if (record->event.pressed) {
-                if (get_mods() & MOD_MASK_SHIFT) {
-                    layer_invert(_LEDS);
-                    funleds_shift_used = true;
-                } else {
-                    funleds_shift_used = false;
-                    layer_on(_FUN);
-                }
-            } else {
-                if (!funleds_shift_used) {
-                    layer_off(_FUN);
-                }
+        case KC_BSPC:
+            if (record->event.pressed && (get_mods() & MOD_MASK_SHIFT)) {
+                tap_code(KC_DEL);
+                return false;
             }
-            return false;
+            return true;
+        case KC_DEL:
+            if (record->event.pressed && (get_mods() & MOD_MASK_SHIFT)) {
+                layer_invert(_LEDS);
+                return false;
+            }
+            return true;
     }
     return true;
 }
